622e863c54b8bd5ea282b31913742c99ef32dcfa
ake <tamiratake@gmail.com>
2024-11-18 00:52:01 +0300

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/webhooks/clerk',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
